c89755c9ce25204aed928ac2abcaa53a248992e7
Christopher Guayara <chrisg.deu@gmail.com>
2026-06-27 04:59:49 -0400

    button done

--- Scenes/Interactables/button.gd
+++ Scenes/Interactables/button.gd
@@ -7,12 +7,24 @@ extends Area2D
 
 func _ready() -> void:
 
-	add_to_group("platforms")
+	add_to_group("button")
 
   
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _process(delta: float) -> void:
 
 	pass
+
+  
+
+  
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("trigger"):
+		print("Ive been hit by the kunai")
+	if body.has_method("force_stuck"):
+		body.force_stuck()
+		var rootnode = get_tree().current_scene
+		if rootnode.has_method("door_hit"):
+			rootnode.door_hit()
