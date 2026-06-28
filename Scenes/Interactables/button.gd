3f542ee787f4b5aa3844741cb7b9e738bc41925f
Christopher Guayara <chrisg.deu@gmail.com>
2026-06-28 16:22:50 -0400

    button press

--- Scenes/Interactables/button.gd
+++ Scenes/Interactables/button.gd
@@ -1,13 +1,14 @@
 extends Area2D
 
   
-
-  
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var can_press = true
 
 
 func _ready() -> void:
 
 	add_to_group("button")
+	animation_player.play("init")
 
   
 
@@ -21,6 +22,8 @@ func _process(delta: float) -> void:
   
 
 func _on_body_entered(body: Node2D) -> void:
+	if not can_press : return
+	animation_player.play("pressing")
 	if body.is_in_group("trigger"):
 		print("Ive been hit by the kunai")
 	if body.has_method("force_stuck"):
@@ -28,3 +31,9 @@ func _on_body_entered(body: Node2D) -> void:
 		var rootnode = get_tree().current_scene
 		if rootnode.has_method("door_hit"):
 			rootnode.door_hit()
+	can_press = false
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "pressing":
+		animation_player.play("pressed")
