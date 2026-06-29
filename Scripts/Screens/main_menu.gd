903634cb5280460cdf07d5f312e1a651ac7f4cff
ParkHyun122 <parkhyun122@gmail.com>
2026-06-29 17:09:27 +0900

    deadzone trap made, its called udead

--- Scripts/Screens/main_menu.gd
+++ Scripts/Screens/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels/firstlevel.tscn")
 
 
 
